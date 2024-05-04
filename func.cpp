986217170c04d1c2f8c66ac8c485490d784e42ce
chesswondo <chesswondo@gmail.com>
2024-05-05 00:20:40 +0300

    Added report and comments

--- func.cpp
+++ func.cpp
@@ -1,5 +1,3 @@
-// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <thread>
@@ -9,12 +7,13 @@
 
 using namespace std;
 
+// Function f
 int f(int x)
 {
     this_thread::sleep_for(28s);
     return 421;
 }
-
+// Function g
 int g(int x)
 {
     this_thread::sleep_for(3s);
@@ -28,6 +27,7 @@ int main(int argc, char * argv[])
         cerr << "Usage: funt port f|g\n";
         return 1;
     }
+    // Command-line options
     unsigned short int port = (unsigned short int)atoi(argv[1]);
     int (*func)(int) = (argv[2][0] == 'f') ? f : g;
 
@@ -38,11 +38,11 @@ int main(int argc, char * argv[])
         if (WSAStartup(wVersionRequested, &wsaData) != 0)
             throw runtime_error("WSAStartup");
 
-        // Создание сокета
+        // Socket creation
         ms = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
         if (ms == INVALID_SOCKET) throw runtime_error("socket creation");
 
-        // Приклепление к порту
+        // bind to port 
         sockaddr_in name;
         memset(&name, 0, sizeof(name));
         name.sin_addr.s_addr = INADDR_ANY;
@@ -53,18 +53,20 @@ int main(int argc, char * argv[])
 
         if (listen(ms, 1) != 0) throw runtime_error("listen");
 
-        // Ожидание соединения
+        // Wait for connect
         cs = accept(ms, NULL, NULL);
         if (cs == INVALID_SOCKET) throw runtime_error("accept");
 
-        // Начинается работа
+        // Receive x and ...
         int x;
         int rc = recv(cs, (char*)&x, sizeof(x), 0);
         if (rc < 0) throw runtime_error("recv data");
         if (rc == 0) throw runtime_error("connection closed");;
 
+        // ...caclulation...
         rc = func(x);
 
+        // ...and send answer
         rc = send(cs, (char*)&rc, sizeof(rc), 0);
         if (rc != sizeof(rc)) throw("send data");
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 #define  _CRT_SECURE_NO_WARNINGS
 #define  _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
@@ -11,19 +9,21 @@
 
 using namespace std;
 
-mutex outmutex;
+mutex outmutex;  // Mutex for outputs
 
+// flags "received" and received data
 atomic<bool> received[2] = { false, false };
 atomic<bool> result[2]   = { false, false };
 
 
+// thread function
 void getValue(int x, unsigned short int port, int index)
 {
-    // Создание сокета
+    // Create socket
     SOCKET s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (s == INVALID_SOCKET) throw runtime_error("socket creation");
 
-    // Вызов
+    // Call 
     sockaddr_in name;
     memset(&name, 0, sizeof(name));
     name.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -36,16 +36,17 @@ void getValue(int x, unsigned short int port, int index)
         lock_guard<mutex> lock(outmutex);
         cout << "Send " << x << " to port " << port << "\n";
     }
-
+    // Send x
     if (send(s,(char*)&x,sizeof(x),0) != sizeof(x)) throw runtime_error("send");
 
-    // Получение ответа
+    // Receive result
     if (recv(s,(char*)&x,sizeof(x),0) != sizeof(x)) throw runtime_error("recv");
 
     {
         lock_guard<mutex> lock(outmutex);
         cout << "Recv " << x << " from port " << port << "\n";
     }
+    // Write result and flag
     result[index]   = bool(x);
     received[index] = true;
     closesocket(s);
@@ -54,9 +55,11 @@ void getValue(int x, unsigned short int port, int index)
 int main(int argc, char * argv[])
 {
     int x = 1, y = 2;
+    // x and y can be passed on the command line
     if (argc > 1) x = atoi(argv[1]);
     if (argc > 2) y = atoi(argv[2]);
 
+    // Run f(x) and g(x) processes
     unsigned short int portF = 20120, portG = 20121;
     char portS[20];
     sprintf(portS,"%hd",portF);
@@ -71,14 +74,16 @@ int main(int argc, char * argv[])
         if (WSAStartup(wVersionRequested, &wsaData) != 0)
             throw runtime_error("WSAStartup");
 
+        // Threads for f(x) and g(x)
         thread(getValue,x,portF,0).detach();
         thread(getValue,y,portG,1).detach();
 
-        int Result = -1;
+        int Result = -1;       // -1 => no result
         bool quest = true;
 
         for(int i = 1;;++i)
         {
+            // is received data enough?
             if (received[0] && received[1])
             {
                 Result = result[0] || result[1];
@@ -92,7 +97,7 @@ int main(int argc, char * argv[])
                 Result = 1;
             }
 
-            if (Result != -1)
+            if (Result != -1) // Yes, we have result
             {
                 lock_guard<mutex> lock(outmutex);
                 cout << "f(" << x << ") || g(" << y << ") == "
@@ -100,15 +105,17 @@ int main(int argc, char * argv[])
                 break;
             }
 
+            // No, result not ready. Wait 1 s
             this_thread::sleep_for(1s);
+            // Every 10 s if flag 'quest' is true
             if (i%10 == 0 && quest)
             {
                 lock_guard<mutex> lock(outmutex);
                 cout << "Too long wait. 1 - abort, 2 - retry, 3 - no more questions: ";
                 int n;
                 cin >> n;
-                if (n == 3) quest = false;
-                else if (n == 2) cout << "retry\n";
+                if (n == 3) quest = false;           // no more qurestions
+                else if (n == 2) cout << "retry\n";  // retry
                 else if (n == 1) throw runtime_error("user choose abort");
                 else cout << "wrong choice, retry\n";
             }
